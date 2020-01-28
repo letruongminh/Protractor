8f702005a60795d2bf1038cdb3a22d1dba3b4c0c
minhle <minhlt97.uit@gmail.com>
2020-01-28 13:46:02 +0700

    Demo Protractor App with new scripts

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -1,8 +1,15 @@
 describe('Protractor Demo App', () => {
-  it('Navigate to Supe Calculator App', () => {
+  it('Navigate to Super Calculator App', () => {
     browser.get('http://juliemr.github.io/protractor-demo/');
-    expect(browser.getTitle()).toEqual('Super Calculator');  
+    expect('Super Calculator').toEqual(browser.getTitle());  
   });
 
+  it('one plus two', () => {
+    element(by.model('first')).sendKeys(1); 
+    element(by.model('second')).sendKeys(2); 
+    element(by.id('gobutton')).click();
+    
+    expect(element(by.binding('latest')).getText()).toEqual(5); 
+  });
   
 }); 
