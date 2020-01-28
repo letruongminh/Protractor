2bb25fca1d4db625555113ce2776063875478e7c
minhle <minhlt97.uit@gmail.com>
2020-01-28 14:25:55 +0700

    New commit returning multiple ElementFinders object

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -12,6 +12,7 @@ describe('Protractor Demo App', () => {
     var webTitle = browser.getTitle(); 
     expect( webTitle ).toEqual('Super Calculator'); 
   }); 
+
   it('one plus two', () => {
     firstNumber.sendKeys(1); 
     secondNumber.sendKeys(2); 
@@ -20,4 +21,34 @@ describe('Protractor Demo App', () => {
     expect( latestRes.getText()).toEqual('3'); 
   });
   
+  it('Verify the history', () => {
+    
+  });
+}); 
+
+describe('Verify the history of the app', () => {
+  var firstNumber = element(by.model('first')); 
+  var secondNumber = element(by.model('second')); 
+  var goButton = element(by.id('gobutton')); 
+  var history = element.all(by.repeater('result in memory')); 
+
+  beforeEach(() => {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  }); 
+  
+  function add(a, b){
+    firstNumber.sendKeys(a); 
+    secondNumber.sendKeys(b); 
+    goButton.click(); 
+  }
+
+  it('Verify the history', () => {
+    add(1, 2); 
+    add(5, 6); 
+
+    expect(history.count()).toEqual(2); 
+
+    add(5, 6); 
+    expect(history.count()).toEqual(3); 
+  });
 }); 
